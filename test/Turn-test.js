c85d0f6a56f7a771f7d01b3bf6db7085efc315ad
Taras Tarlov <ttarlov@gmail.com>
2020-01-28 15:44:58 -0700

    Add syntax changes to Turn-test file

---
+++ test/Turn-test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should take in two arguments: string and card obj', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('pug', card);
+    expect(turn.returnGuess()).to.equal('pug');
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it('should check if the users answer is correct', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
+
+  it('It should give feedback on the answer', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.evaluateGuess()).to.equal(true);
+    // console.log(turn.giveFeedback());
+    expect(turn.giveFeedback()).to.equal("You got it! Nice Job");
+  });
+
+});
